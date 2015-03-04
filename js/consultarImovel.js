47718bc524da0e3c87a4480c06d7d7fe80b35329
Bruno Guazina <bruno@guazina.com>
2015-03-04 10:48:46 -0400

    ajustes de html e no js

--- js/consultarImovel.js
+++ js/consultarImovel.js
@@ -1,17 +1,13 @@
 //
-function ConsultarImovel(){
-	$.getJSON("imovel/0001.txt", function(dados) {
-	               
-	    $("#tipo").prepend(dados.tipo);
-	    $("#bairro").append(dados.bairro);
-	    $("#cidade").append(dados.localidade);
-	    tratarImagemPrincipal(dados.id);
-	    tratarValorDoImovel(dados.valor);
-		
-	});
-};
-
-ConsultarImovel();
+$.getJSON("imovel/0001.txt", function(dados) {
+               
+    $("#tipo").prepend(dados.tipo);
+    $("#bairro").append(dados.bairro);
+    $("#cidade").append(dados.localidade);
+    tratarImagemPrincipal(dados.id);
+    tratarValorDoImovel(dados.valor);
+	
+});
 
 function tratarImagemPrincipal(idDoImovel) {
 	$("img, #imagemPrincipal").append("<img src='img/"+idDoImovel+"/principal.jpg' width='100%'>")
