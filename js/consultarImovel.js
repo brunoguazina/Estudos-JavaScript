125e076fd739da2344d6122d12adcc3fa2fd48e8
Bruno Guazina <bruno@guazina.com>
2015-03-04 09:47:15 -0400

    incrementando json do imovel

--- js/consultarImovel.js
+++ js/consultarImovel.js
@@ -1,13 +1,24 @@
-
+//
 function ConsultarImovel(){
-		$.getJSON("imovel/casa.json", function(dados) {
-	                   
-	        $("#tipo").html(dados.tipo);
-	        $("#valor").html(dados.valor);
-	        $("#bairro").html(dados.bairro);
-	        $("#cidade").html(dados.localidade);
-	                    
+	$.getJSON("imovel/0001.txt", function(dados) {
+	               
+	    $("#tipo").prepend(dados.tipo);
+	    $("#bairro").append(dados.bairro);
+	    $("#cidade").append(dados.localidade);
+	    tratarImagemPrincipal(dados.id);
+	    tratarValorDoImovel(dados.valor);
+		
 	});
 };
 
 ConsultarImovel();
+
+function tratarImagemPrincipal(idDoImovel) {
+	$("img, #imagemPrincipal").append("<img src='img/"+idDoImovel+"/principal.jpg' width='100%'>")
+}
+
+function tratarValorDoImovel(valorDoImovel) {
+	//TODO: tratar o valor do imovel
+	var valorDoImovelTratado = valorDoImovel;
+	$("#valor").html("R$ " + valorDoImovelTratado + ",00");
+}
