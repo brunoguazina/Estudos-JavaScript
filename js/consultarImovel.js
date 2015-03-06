877c9a8f8c41cb64887afc4e9b6260c2fa6e3408
Bruno Guazina <bruno@guazina.com>
2015-03-06 11:16:09 -0400

    inclusao da cidade e estado na localidade

--- js/consultarImovel.js
+++ js/consultarImovel.js
@@ -3,14 +3,14 @@ $.getJSON("imovel/0001.txt", function(dados) {
                
     $("#tipo").prepend(dados.tipo);
     $("#bairro").append(dados.bairro);
-    $("#cidade").append(dados.localidade);
+    $("#localidade").append(dados.localidade, " - " , dados.uf);
     tratarImagemPrincipal(dados.id);
     tratarValorDoImovel(dados.valor);
 	
 });
 
 function tratarImagemPrincipal(idDoImovel) {
-	$("img, #imagemPrincipal").append("<img src='img/"+idDoImovel+"/principal.jpg' width='100%'>")
+	$("img, #imagemPrincipal").append("<img src='img/"+idDoImovel+"/principal.jpg' width='100%'>");
 }
 
 function tratarValorDoImovel(valorDoImovel) {
