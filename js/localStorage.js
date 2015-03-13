1a4d3f58c5f48faa574aecf7e941e448a2a95365
brunoguazina <bruno@guazina.com>
2015-03-13 10:06:10 -0400

    criando variable global para pessoas

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,40 +1,42 @@
 
-
 if (localStorage.getItem("Pessoas") == null) {
 		localStorage.setItem( "Pessoas" ,JSON.stringify([]));
-}
+	}
 
+var pessoas = "";
+
+$(window).load(function() {
+
+	pessoas = JSON.parse(localStorage.getItem("Pessoas"));
+	listar(pessoas);
+	return pessoas;
+});
 
 
 $("#salvar").on("click", function() {
 
-	var objeto = {
+	var pessoa = {
 		nome: $("#nome").val(),
 		idade: $("#idade").val(),
 		profissao: $("#profissao").val(),
 	};
 
-	salvar(objeto);
-
+	salvar(pessoa);
 });
 
 
 function salvar(pessoa) {
-	var pessoas = JSON.parse(localStorage.getItem("Pessoas"));
+	
 	pessoas.push(pessoa);
 	localStorage.setItem( "Pessoas" ,JSON.stringify(pessoas));
-	listar(pessoas);
 }
-
+ 
 
 function listar(pessoas) {
 	
 	pessoas.forEach(function(pessoa) {
-    	
     	$("#listaDePessoas").append("<li>" + "Nome: " + pessoa.nome + "</li>");
-
   	});
-
 }
 
 
